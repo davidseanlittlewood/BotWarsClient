2d897793d738428a7ed1c2b98574d4478b89f11d
davidseanlittlewood <davidseanlittlewood@gmail.com>
2018-11-06 08:26:32 +0000

    Create BotWarsClient

---
+++ BotWarsClient/BotWarsClient/Bots/VanillaBot.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotWarsClient.Bots
+{
+    public class VanillaBot : BotBaseClass
+    {
+        public string OpponentName { get; set; }        
+        public int Health { get; set; }
+        public int Flips { get; set; }
+        public int Fuel { get; set; }
+        public int FlipOdds { get; set; }
+        public int ArenaSize { get; set; }
+        public char Direction { get; set; }
+        public bool Flipped { get; set; }
+        public bool OpponentFlipped { get; set; }
+
+        public VanillaBot()
+        {            
+        }
+
+        /// <summary>
+        /// Tell the server what we'd like to do in our move
+        /// </summary>
+        public override Move GetMove()
+        {
+            return Move.MoveBackwards; // This bot isn't very smart - it'll pick a walk backwards until it falls out of the arena and dies
+        }
+
+        public override void SetStartValues(string opponentName, int health, int arenaSize, int flips, int flipOdds, int fuel, char direction, int startIndex)
+        {
+            OpponentName = opponentName;
+            Health = health;
+            ArenaSize = arenaSize;
+            Flips = flips;
+            FlipOdds = flipOdds;
+            Fuel = fuel;
+            Direction = direction;
+            Flipped = false;
+            OpponentFlipped = false;
+
+            base.SetStartValues(opponentName, health, arenaSize, flips, flipOdds, fuel, direction, startIndex);
+        }
+
+        public override void CaptureOpponentsLastMove(Move lastOpponentsMove)
+        {
+                     
+        }
+
+        public override void SetFlippedStatus(bool flipped)
+        {
+
+        }
+        public override void SetOpponentFlippedStatus(bool opponentFlipped)
+        {
+
+        }
+
+
+    }
+}
---
+++ BotWarsClient/BotWarsClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using BotWarsClient.Bots;
+using System;
+
+namespace BotWarsClient
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int port = 5999; //set port initially to 5999
+
+            if (args.Length > 0)
+            {
+                port = (Convert.ToInt32(args[0]));
+            }
+
+
+            HttpListenerClass httpListener = new HttpListenerClass(port, new VanillaBot());
+        
+        }
+    }
+}
